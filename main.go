61b75277f0be58d4bf6e082aa103bfc03a525173
yangsgit <liyanghehe@gmail.com>
2018-06-16 12:13:14 -0400

    change url

--- main.go
+++ main.go
@@ -245,7 +245,7 @@ const (
 	//PROJECT_ID = "around-xxx"
 	//BT_INSTANCE = "around-post"
 	// Needs to update this URL if you deploy it to cloud.
-	ES_URL      = "http://35.196.159.6:9200"
+	ES_URL      = "http://35.190.154.169:9200"
 	BUCKET_NAME = "post-image-343240730592"
 	PROJECT_ID  = "annular-net-206502"
 	BT_INSTANCE = "around-post"
